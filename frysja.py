61d690c82fd6db5496abaa635a909c5b3793efad
oysch <89084403+oysch@users.noreply.github.com>
2021-11-17 20:30:45 +0100

    Update frysja.py

--- frysja.py
+++ frysja.py
@@ -2,16 +2,22 @@ from datetime import date, datetime
 import datetime
 import urllib.request
 
-dato = datetime.datetime.utcnow().strftime("%Y-%m-%d_%H-%M")
+# Get date today for the filename
+date = datetime.datetime.utcnow().strftime("%Y-%m-%d_%H-%M")
 
+# Set start date
 start = date(2021, 10, 19)
-print(start)
-today= date.today()
-diff=today-start
 
+# Get date today
+today= date.today()
+
+# Calculate the diff between start date and date today
+diff=today-start
 diff = str(diff.days)
 
+# Adds zeros (0) at the beginning of the file name
 filnavn = diff.zfill(4)
 
+# Insert image URL
 urllib.request.urlretrieve("https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg", 
-filnavn+"_frysja_"+str(dato)+".jpg")
+filnavn+"_frysja_"+str(date)+".jpg")
